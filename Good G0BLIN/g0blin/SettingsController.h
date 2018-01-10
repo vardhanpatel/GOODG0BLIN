36bdaaffd09000bebdd13e741f2a27db810bf631
Vardhan Patel <vardhanpatel99@gmail.com>
2018-01-09 23:04:59 -0500

    Project Upload

---
+++ Good G0BLIN/g0blin/SettingsController.h
@@ -0,0 +1,17 @@
+//
+//  SettingsController.h
+//  g0blin
+//
+//  Created by Sticktron on 2017-12-30.
+//  Copyright © 2017 Sticktron. All rights reserved.
+//GUI CHANGE
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsController : UIViewController
+@property (weak, nonatomic) IBOutlet UISwitch *drakmode;
+@property (weak, nonatomic) IBOutlet UISwitch *reinstallBootstrapSwitch;
+
+
+
+@end
